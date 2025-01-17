6090acc424332b36cb77f686d8bc93f4279fcb54
YosephAwoke <yosephawoke8@gmail.com>
2025-01-17 15:32:28 +0300

    Animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,15 @@ import { HERO_CONTENT } from "../constants";
 import profilephoto from "../assets/josX.PNG";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import PropTypes from "prop-types";
+import { PiRectangleThin } from "react-icons/pi";
 
 const Hero = ({ scrollToContact }) => {
   return (
-    <div className="border-b border-neutral-900 pb-12 mt-28 lg:mb-25">
+    <div className="border-b border-neutral-900 pb-15 pt-20 mt-32 lg:mb-25">
       <div className="flex flex-wrap">
         <div className=" lg:w-1/2">
           <div className="w-full flex flex-col item-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-20 lg:text-8xl">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-10 lg:text-8xl">
               Yoseph Awoke
             </h1>
             <span
@@ -32,9 +33,12 @@ const Hero = ({ scrollToContact }) => {
           </div>
           
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <div className="w-full lg:w-1/2 lg:p-8 h-full flex items-center justify-center ">
           <div className="flex justify-center animate-updown">
             <img src={profilephoto} alt="Yoseph Awoke" />
+          </div>
+          <div className="absolute -z-10 flex justify-center items-center animate-pulse">
+            <PiRectangleThin className="md:h-[80%] sm:h-[110%] min-h-[600px] w-auto text-cyan-300 blur-lg animate-[spin_30s_linear_infinite]"/>
           </div>
         </div>
       </div>
