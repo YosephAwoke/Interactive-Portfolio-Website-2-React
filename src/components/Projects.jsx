c85135b91011cc51b6d452fc48d4d1396e05c897
YosephAwoke <yosephawoke8@gmail.com>
2025-01-27 17:19:23 +0300

    subhirosection

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,7 @@
 import { Col, Container, Nav, Tab, Row } from "react-bootstrap";
 // import {ProjectCard} from "./ProjectCard";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-// import projImg1 from "../assets/img/project-img1.png";
-// import projImg2 from "../assets/img/project-img2.png";
-// import projImg3 from "../assets/img/project-img3.png";
-// import projImg4 from "../assets/img/project-img1.png";
-// import 'animate.css';
+
 import TrackVisibility from "react-on-screen";
 import { ProjectsMade } from "./ProjectsMade";
 
--- src/components/SubHiroSection.jsx
+++ src/components/SubHiroSection.jsx
@@ -2,10 +2,10 @@
 
 const SubHiroSection = () => {
   return (
-    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl  md:text-2xl sm:text-4xl py-8'>
-      <p>Fast Learner</p>
-      <p>Team Work</p>
-      <p>Creative</p>
+    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl  md:text-2xl sm:text-4xl py-8 items-center gap-4 bg-brown'>
+      <p className="md:block sm:hidden">Fast Learner</p>
+      <p className="md:block sm:hidden">Team Work</p>
+      <p >Creative</p>
     </div>
   )
 }
