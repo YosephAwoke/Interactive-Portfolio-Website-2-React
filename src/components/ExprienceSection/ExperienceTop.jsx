eb2a128d4997910867ac7c3bf0fbfaa276c641a9
YosephAwoke <yosephawoke8@gmail.com>
2025-01-30 17:11:38 +0300

    Experience section updated

--- src/components/ExprienceSection/ExperienceTop.jsx
+++ src/components/ExprienceSection/ExperienceTop.jsx
@@ -1,15 +1,15 @@
 import ExperienceTopLeft from "./ExperienceTopLeft"
-// import ExperienceTopMiddle from "./ExperienceTopMiddle"
-// import ExperienceTopRight from "./ExperienceTopRight"
+import ExperienceTopMiddle from "./ExperienceTopMiddle"
+import ExperienceTopRight from "./ExperienceTopRight"
 
 
 const ExperienceTop = () => {
   return (
-    <div>
+    <div className="flex sm:flex-col lg:flex-row gap-4 items-center jestify-center">
      
       <ExperienceTopLeft />
-      {/* <ExperienceTopMiddle />
-      <ExperienceTopRight />  */}
+      <ExperienceTopMiddle />
+      <ExperienceTopRight />
     </div>
   )
 }
